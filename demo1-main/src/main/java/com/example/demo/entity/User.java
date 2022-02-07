6b6dc15470d0dd4a9743d0a08b5bb6736ee21a86
Akruti97 <98221127+Akruti97@users.noreply.github.com>
2022-02-07 16:48:45 +0530

    Add files via upload

---
+++ demo1-main/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,59 @@
+package com.example.demo.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Range;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	@Range(min = 0)
+	private Integer age;
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "address_id",referencedColumnName = "id")
+	Address address;
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	@NotBlank
+	private String name;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
